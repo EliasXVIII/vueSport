0c1c1e7a8752c7224b1f6f0d78674aec0f055883
jonathan-perez <jonathanibanez86@gmail.com>
2024-03-27 11:27:07 +0100

    created card components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div  class="bg-green">
     <Navbar />
+    <card />
 
      <!-- Router View -->
      <router-view/>
@@ -9,6 +10,7 @@
 
 <script setup>
 import Navbar from './components/ui/Navbar.vue'
+import card from './components/ui/card.vue'
 
 </script>
 
