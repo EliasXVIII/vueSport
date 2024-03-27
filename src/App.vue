2ab3ae598648fbde098cbbb85433eff0c613592a
oussama2505 <oussama.brahmi0608@gmail.com>
2024-03-27 23:03:22 +0100

    Install and Configuration of Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
+<!-- App.vue -->
 <template>
-  <div  class="bg-green">
-    <Navbar />
-
-     <!-- Router View -->
-     <router-view/>
+  <div class="bg-green">
+    <!-- <router-link to="/">Home</router-link>
+    <router-link to="/hiking">Hiking</router-link>
+    <router-link to="/biking">Biking</router-link> -->
+    <Navbar/>
+    <!-- Router View -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script setup>
-import Navbar from './components/ui/Navbar.vue'
+import Navbar from './components/ui/Navbar.vue';
 
 </script>
-
--- src/main.js
+++ src/main.js
@@ -1,25 +1,32 @@
-import { createApp } from 'vue'
+// main.js
 import './style.css'
-import App from './App.vue'
-
-
-
-// Font Awesome imports
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faHome, faHiking, faBiking } from '@fortawesome/free-solid-svg-icons'
-
-import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
+import App from './App.vue'
+import HomeView from './views/HomeView.vue'
+import Hiking from './views/Hiking.vue'
+import Biking from './views/Biking.vue'
 
 // Add icons to the library
 library.add(farStar);
+library.add(faHome, faHiking, faBiking);
 
-library.add(faHome, faHiking, faBiking)
-
-
-
+// Create router
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', name: 'Home', component: HomeView },
+        { path: '/hiking', name: 'Hiking', component: Hiking },
+        { path: '/biking', name: 'Biking', component: Biking },
+    ]
+});
 
 // Create Vue application and use router
 createApp(App)
-.component('font-awesome-icon', FontAwesomeIcon)
-.mount('#app')
+    .use(router)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app');
