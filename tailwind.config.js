422165d05e0046e755e8315c8f93b159686063b0
oussama2505 <oussama.brahmi0608@gmail.com>
2024-03-27 01:08:18 +0100

    Create Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import {'tailwindcss'}.config} */
+module.exports = {
+  content:[
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'custom-gray': '#A6A485',
+        'custom-green': '#3F402B',
+        // Add your custom background color here
+      },
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+      },
+    },
+    },
+  
+  plugins: [],
+}
+
+
