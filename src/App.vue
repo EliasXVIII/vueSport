4f64eac3084071697aa5d28f6604182511dd438a
abderrahmanehichou <abderrahmanehichou06@gmail.com>
2024-03-27 12:48:34 +0100

    create hiking card

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div  class="bg-green">
     <Navbar />
+    <HikingCard />
 
      <!-- Router View -->
      <router-view/>
@@ -9,6 +10,6 @@
 
 <script setup>
 import Navbar from './components/ui/Navbar.vue'
-
+import HikingCard from './components/ui/HikingCard.vue'
 </script>
 
