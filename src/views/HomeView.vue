6bcc8e03d6694ea3612cc981b02e63ab47d55ce1
oussama2505 <oussama.brahmi0608@gmail.com>
2024-04-05 18:30:03 +0200

    Google Map Component Configurations

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,10 @@
     <h1>This is the Home View</h1>
     <p>Content coming soon...</p>
     <Card/>
-    <MapsRoutes/>
   </div>
 </template>
 
 <script setup>
 import Card from '../components/ui/card.vue';
-import MapsRoutes from '../components/ui/MapsRoutes.vue'
 // Component logic goes here
 </script>
