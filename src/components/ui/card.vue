09cfc9a5d71a9186a4c3bf126c08903eb10d4304
jonathan-perez <jonathanibanez86@gmail.com>
2024-04-12 10:22:21 +0200

    card

--- src/components/ui/card.vue
+++ src/components/ui/card.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="max-w-sm bg-custom-oscuro border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-12 mr-10%">
+  <div class="max-w-sm bg-custom-oscuro border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-12 mr-10%">
+    <img src="/src/assets/images/biking2.jpg" alt="" class="mt-2 mb-1"> <!-- Reducir espacio entre imagen y título -->
+    <div class="p-5 flex flex-col items-center">
       <a href="#">
-        <img class="rounded-t-lg" :src="imagePath" alt="" @error="handleImageError" />
+        <h5 class="mb-4 text-2xl font-bold tracking-tight text-white dark:text-white">Muskiz-Pobeña</h5> <!-- Aumentar espacio entre título y botón -->
       </a>
-      <div class="p-5">
-        <a href="#">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">{{ Rutas__sendero }}</h5>
-        </a>
-        <button type="button" @click="handleButtonClick" class="text-white hover:text-black border border-white hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-black dark:hover:text-black dark:hover:bg-gray-600 dark:focus:ring-gray-800">
-          Enter
-        </button>
-        <p v-if="errorMessage" class="text-red-500">{{ errorMessage }}</p>
-      </div>
+      <button type="button" @click="handleButtonClick" class="text-white hover:text-black border border-white hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 mt-4 dark:border-gray-600 dark:text-black dark:hover:text-black dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+        Enter
+      </button>
+      <p v-if="errorMessage" class="text-red-500">{{ errorMessage }}</p>
     </div>
-  </template>
+  </div>
+</template>
+
+
   
   <script>
   import { defineComponent } from 'vue';
