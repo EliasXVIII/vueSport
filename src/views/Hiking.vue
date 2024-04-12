05dd68e1bbc1a6314a7337ad188e410ad2d8bfd4
oussama2505 <oussama.brahmi0608@gmail.com>
2024-04-13 00:14:58 +0200

    Modify Hiking Component

--- src/views/Hiking.vue
+++ src/views/Hiking.vue
@@ -19,11 +19,11 @@
   <!-- Formulario y contenido que se desplaza sobre la imagen -->
   <form class="max-w-sm mx-auto relative z-20">
   <div class="mb-5">
-    <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Where?</label>
+    <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose Distance:</label>
     <input type="text" id="text" class="shadow-sm bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"  required />
   </div>
   <div class="mb-5">
-    <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose Distance</label>
+    <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose Difficulty</label>
     <input type="text" id="text" class="shadow-sm bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
   </div>
   <div style="margin-bottom: 20px;display: flex; justify-content: center;">
