a34bd1c420401f7edc7f4b7608bb77c79f403f0f
oussama2505 <oussama.brahmi0608@gmail.com>
2024-04-16 10:22:30 +0200

    card modifing

--- src/components/ui/Card.vue
+++ src/components/ui/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-sm bg-custom-oscuro border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-12 mr-10%">
-    <img :src="imagePath" alt="Hiking route" class="mt-2 mb-1 w-full max-w-xs h-auto mx-auto my-auto">
+    <img src="../assets/images/biking6.jpg" alt="Hiking route" class="mt-2 mb-1 w-full max-w-xs h-auto mx-auto my-auto">
     <div class="p-5 flex flex-col items-center">
       <a :href="routeUrl">
         <h5 class="mb-4 text-2xl font-bold tracking-tight text-white dark:text-white">{{ title }}</h5>
