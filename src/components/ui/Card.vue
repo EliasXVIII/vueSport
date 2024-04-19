8ceb9c7d3fd8ec5ddbc2e1921d4311658d1a6fa7
Annageorgia <anatsomaia@gmail.com>
2024-04-19 14:16:45 +0200

    sticky of map and shadows for the cards

--- src/components/ui/Card.vue
+++ src/components/ui/Card.vue
@@ -1,18 +1,15 @@
 
- <template>
-  <div class="max-w-sm bg-custom-oscuro border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-12 mr-10%">
+<template>
+  <div class="max-w-sm bg-custom-oscuro border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 mt-12 mr-10%  shadow-lg shadow-rose-950 ">
     <div class="relative w-full h-0" style="padding-bottom: 100%;">
-     
       <a target="_blank" :href="routeUrl">      
         <img :src="imagePath" alt="Hiking route" class="absolute inset-0 w-full h-full object-cover rounded-t-lg">
       </a>
-      </div>
+    </div>
     <div class="p-5">
-      
-        <h5 class="mb-2 text-xl font-bold tracking-tight text-white dark:text-white">{{ title }}</h5>
-      
-      <p class="text-sm font-semibold text-rose-500 ">Duration aproximate: {{ duration }} Hs</p>
-      <p class="text-sm font-semibold text-rose-500 ">Distance: {{ distance }} Km</p>
+      <h5 class="mb-2 text-xl font-bold tracking-tight text-white dark:text-white">{{ title }}</h5>
+      <p class="text-sm font-semibold text-rose-500">Duration approximate: {{ duration }} Hs</p>
+      <p class="text-sm font-semibold text-rose-500">Distance: {{ distance }} Km</p>
     </div>
   </div>
 </template>
