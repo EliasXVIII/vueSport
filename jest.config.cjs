0a59d292d7652cd93b3821d1e6da42d549add29f
EliasJavierRiquelme <elias.renault4@gmail.com>
2024-04-20 18:20:48 +0200

    Config test and tests fail

---
+++ jest.config.cjs
@@ -0,0 +1,13 @@
+
+/* module.exports = {
+  '^.+\\.js$': 'babel-jest',
+  preset: '@vue/cli-plugin-unit-jest/presets/no-babel'
+}
+
+ */
+module.exports = {
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+  preset: '@vue/cli-plugin-unit-jest/presets/no-babel'
+};
